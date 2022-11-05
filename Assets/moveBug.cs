bd9346674dc6f758831ca8f2a980ddfd9b177edd
Michelle Wang <54217965+skyleapa@users.noreply.github.com>
2022-11-04 23:50:31 -0700

    bugs move back and forth
    bugged bugs are fixed, refactored to one moveBug script, modifiable for x and y movement

---
+++ Assets/moveBug.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBug : MonoBehaviour
+{
+    public float speed = 5f;
+    public double constraintLeft;
+    public double constraintRight;
+
+    public double constraintUp;
+    public double constraintDown;
+
+    public bool isVertical;
+
+    bool switc = true;
+
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isVertical) {
+            if (switc) {
+                moveright();
+            }
+            if (!switc) {
+                moveleft();
+            }
+            if (transform.position.x >= constraintRight) {
+                switc = false;
+            }
+            if (transform.position.x <= constraintLeft) {
+                switc = true;
+            }
+        } else {
+            if (switc) {
+                movedown();
+            }
+            if (!switc) {
+                moveup();
+            }
+            if (transform.position.y >= constraintUp) {
+                switc = true;
+            }
+            if (transform.position.y <= constraintDown) {
+                switc = false;
+            }
+        }
+    }
+
+    void moveright() {
+        transform.Translate(speed*Time.deltaTime,0,0);
+    }
+
+    void moveleft() {
+        transform.Translate(-speed*Time.deltaTime,0,0);
+    }
+
+    void moveup() {
+        transform.Translate(0,speed*Time.deltaTime,0);
+    }
+
+    void movedown() {
+        transform.Translate(0,-speed*Time.deltaTime,0);
+    }
+}
