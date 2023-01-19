9d4c63d0591e4e3cad172f0b93d9d1efdb5d2d72
colline <collineng@gmail.com>
2023-01-18 19:47:55 -0800

    feat: can rotate the blocks

--- Assets/Scripts/TetrisBlock.cs
+++ Assets/Scripts/TetrisBlock.cs
@@ -10,31 +10,45 @@ public class TetrisBlock : MonoBehaviour{
     public GridSprite gridSprite;
     public SpriteRenderer spriteRenderer;
     private float cellSize;
+    private bool isSelected;
 
-    void Awake(){
+    void Awake() {
         cellSize = 1.0f;
     }
-    void Start(){
+    void Start() {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void OnMouseDrag(){
-        spriteRenderer.sortingOrder = 1;
-        transform.position = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
-        transform.localScale = new Vector2(cellSize + 0.2f, cellSize+ 0.2f);
+    void Update()
+    {
+        if (isSelected) {
+            if (Input.GetButtonDown("RotateClockwise"))
+                transform.Rotate(0, 0, -90);
+
+            if (Input.GetButtonDown("RotateCounterClockwise"))
+                transform.Rotate(0, 0, 90);
+        }
     }
 
-    void OnMouseUpAsButton(){
+    void OnMouseDrag() {
+        isSelected = true;
+        spriteRenderer.sortingOrder = 1;
+        transform.position = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
+        transform.localScale = new Vector2(cellSize + 0.2f, cellSize + 0.2f);
+    }
+    
+    void OnMouseUpAsButton() {
+        isSelected = false;
         spriteRenderer.sortingOrder = 0;
         transform.localScale = new Vector2(cellSize, cellSize);
         PlaceBlock(gridSprite.mouseOverGrid);
     }
 
 
-    private void PlaceBlock(bool onGrid){
-        if(!onGrid){
+    private void PlaceBlock(bool onGrid) {
+        if (!onGrid) {
             transform.position = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
-        }else{
+        } else {
             Vector3Int cellPosition =  grid.WorldToCell(cam.ScreenToWorldPoint(Input.mousePosition));
             transform.position = grid.CellToWorld(cellPosition) + new Vector3(cellSize/2,cellSize/2,cellSize/2);
         }
