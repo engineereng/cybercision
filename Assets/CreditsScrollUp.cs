6297ee4b06dec3e3e9ab19436a1738ba92dd530e
Collin Eng <eng.engineereng@gmail.com>
2023-03-18 17:13:10 -0700

    Update Assets/CreditsScrollUp.cs

--- Assets/CreditsScrollUp.cs
+++ Assets/CreditsScrollUp.cs
@@ -10,12 +10,6 @@ public class CreditsScrollUp : MonoBehaviour
     public float ScrollFinishY = 0;
     public string ExitSceneName = "MainScene";
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
