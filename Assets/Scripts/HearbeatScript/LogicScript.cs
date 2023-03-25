45ef004c202ce8f3ad1659d9fdb8ad60699d4596
Erick Jovan Muljadi <88482247+erickj04@users.noreply.github.com>
2023-03-25 16:55:16 -0700

    Heartbeat (#11)
    * fix video player
    
    * "add delay & change spriteRender to Image"
    
    * add heart beating animation and icons
    
    * add minigame manager

--- Assets/Scripts/HearbeatScript/LogicScript.cs
+++ Assets/Scripts/HearbeatScript/LogicScript.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 public class LogicScript : MonoBehaviour
 {
-    private int heartBPM ;
+    private int heartBPM;
+    private string prevStatus;
     public Text BPMText;
     public GameObject gameOverScreen;
     private float Timer;
@@ -16,35 +17,47 @@ public class LogicScript : MonoBehaviour
     private bool started;
     public GameObject startScreen;
     public VideoManager VideoManager;
+
+    private float delay;
     void Start(){
         VideoManager = FindObjectOfType<VideoManager>();
         heartBPM = 60;
-        Timer = 60f;
+        Timer = 30f;
         started = false;
         FindObjectOfType<AudioManager>().Play("Theme");
+        prevStatus = "Healthy";
+        delay = 0;
+        VideoManager.HeartBeat();
     }
 
     void Update(){
+        delay += Time.deltaTime;
         if(started){
             if(isGameOver()){
                 gameOverScreen.SetActive(true);
-                patientStatus.text = "Patient is " + status();
+                patientStatus.text = "PATIENT STATUS: " + status();
                 FindObjectOfType<AudioManager>().Mute("Theme");
                 VideoManager.Flatline();
+                MinigameManager.GetManager().FinishMinigame(false);
             }
             else if(isWin()){
                 winnerScreen.SetActive(true);
-                patientStatus.text = "Patient is " + status();
+                patientStatus.text = "PATIENT STATUS: " + status();
                 TimerText.text = "TIME: " + Timer.ToString("0");
                 FindObjectOfType<AudioManager>().Mute("Theme");
+                MinigameManager.GetManager().FinishMinigame(true);
             }
             else {
                 Timer -= Time.deltaTime;
-                patientStatus.text = "Patient is " + status();
+                patientStatus.text = "PATIENT STATUS: " + status();
                 TimerText.text = "TIME: " + Timer.ToString("0");
-                BPMText.text = "Beat Per Minute: " + heartBPM.ToString("0");
+                BPMText.text = "BEATS PER MINUTE: \n" + heartBPM.ToString("0");
             }
-            if(VideoManager.checkOver()){
+            if(delay >= 2 && VideoManager.checkOver() && statusChanged()){
+                delay = 0;
+                VideoManager.Pause();
+                Debug.Log("checked");
+                print("change animation");  
                 if(status() == "Dying"){
                     VideoManager.Dying();
                 }
@@ -54,6 +67,8 @@ public class LogicScript : MonoBehaviour
                 else{
                     VideoManager.Healthy();
                 }
+                Debug.Log("changed!!");
+                VideoManager.Play();
             }
         }
         else {
@@ -87,7 +102,7 @@ public class LogicScript : MonoBehaviour
 
     public bool isGameOver(){
         if(heartBPM == 0){
-            BPMText.text = "Beat Per Minute: " + heartBPM.ToString("0");
+            BPMText.text = "BEATS PER MINUTE: \n" + heartBPM.ToString("0");
             return true;
         };
         return false;
@@ -98,23 +113,31 @@ public class LogicScript : MonoBehaviour
 
     public string status(){ 
         if(heartBPM == 0){
-            return "Dead";
+            return "DEAD";
         }
         else if(heartBPM < 30){
-            return "Dying";
+            return "DYING";
         }
         else if(heartBPM < 60){
-            return "Not Well";
+            return "NOT WELL";
         }
         else if(heartBPM < 80){
-            return "Okay";
+            return "OKAY";
         }
         else {
-            return "Healthy";
+            return "HEALTHY";
         }
     }
 
     public bool getStarted(){
         return started;
+    }
+
+    public bool statusChanged(){
+        if(status() != prevStatus){
+            prevStatus = status();
+            return true;
+        }
+        return false;
     }
 }
